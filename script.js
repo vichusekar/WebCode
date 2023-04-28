89f1d380912604a06bedabc676b01f9d3af60b65
Vignesh Rajasekar <127473360+vichusekar@users.noreply.github.com>
2023-04-28 15:21:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,3 @@
-
-
-// // async function brew(){
-// // let res= await fetch("https://api.openbrewerydb.org/v1/breweries/")
-// // let res1=await res.json()
-// // console.log(res1)
-
-// // }
-
-// // console.log("countryname"+data.name)
-
-
-// //Final Code for the Rest countries
-
-
-// var div=document.createElement("div");
-// div.innerHTML="this is div";
-
-// var container=document.createElement("div");
-// container.className="container";
-
-// var row=document.createElement("div");
-// row.classList.add("row","m-3");
-
-// div.append(container,row);
-// document.body.append(div);
-
-
-// async function data(){
-// let result=await fetch("https://api.openbrewerydb.org/v1/breweries/")
-// let data=await result.json();
-// console.log(data)
-// foo()
-// }
-
-// function foo(){
-//     for(var i=0;i<data.length;i++){
-//         row.innerHTML+=`
-//         <div class="col-md-4">
-//         <div class="card border-primary mb-3 " style="width: 18rem;">
-//         <img src="${data[i].phone}" class="card-img-top" alt="">
-//           <div class="card-body">
-//           <h5 class="card-title">Card title</h5>
-//           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//         </div>
-//       </div>
-//       </div>`;
-//     }
-// }
-// data()
-
-
 var container=document.createElement("div");
 container.className="container";
 
@@ -62,9 +10,6 @@ nametype.style.margin="32px";
 nametype.style.fontFamily="Arial";
 
 container.append(row,nametype);
-
-
-
 
 
 async function brew(){
@@ -91,8 +36,7 @@ async function foo(){
       </div>`;
 
       document.body.append(container);
-      // console.log(`Name: ${result1[i].name} ${result1[i].phone} ${result1[i].address_1} ${result1[i].website_url}`)
-      // getdata()
+
 
         }    
     }
@@ -101,11 +45,4 @@ async function foo(){
     }
 }
 
-// async function getdata(){
-//   let gd= await fetch ("https://api.openbrewerydb.org/v1/breweries/")
-//   let gd1= await gd.json()
-//   for(var i=0; i<gd1.length; i++){
-//     console.log(gd1[i].name)
-//   }
-// }
 brew()
